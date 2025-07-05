38be4d9857a2d6e5de05a1854fb94315bfd9f5b3
unknown <missionhach@gmail.com>
2025-07-06 00:19:58 +0530

    done with the changes

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,25 +1,24 @@
-import { Button } from "@/components/ui/button";
+import { canCreateResume } from "@/lib/permissions";
 import prisma from "@/lib/prisma";
+import { getUserSubscriptionLevel } from "@/lib/subscriptions";
 import { resumeDataInclude } from "@/lib/types";
 import { auth } from "@clerk/nextjs/server";
-import { PlusSquare } from "lucide-react";
 import { Metadata } from "next";
-import Link from "next/link";
+import CreateResumeButton from "./CreateResumeButton";
 import ResumeItem from "./ResumeItem";
-
 
 export const metadata: Metadata = {
   title: "Your resumes",
 };
 
 export default async function Page() {
-   const { userId } = await auth();
+  const { userId } = await auth();
 
   if (!userId) {
     return null;
   }
 
-  const [resumes, totalCount] = await Promise.all([
+  const [resumes, totalCount, subscriptionLevel] = await Promise.all([
     prisma.resume.findMany({
       where: {
         userId,
@@ -34,18 +33,16 @@ export default async function Page() {
         userId,
       },
     }),
+    getUserSubscriptionLevel(userId),
   ]);
 
   // TODO: Check quota for non-premium users
 
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
-      <Button asChild className="mx-auto flex w-fit gap-2">
-        <Link href="/editor">
-          <PlusSquare className="size-5" />
-          New resume
-        </Link>
-      </Button>
+      <CreateResumeButton
+        canCreate={canCreateResume(subscriptionLevel, totalCount)}
+      />
       <div className="space-y-1">
         <h1 className="text-3xl font-bold">Your resumes</h1>
         <p>Total: {totalCount}</p>
